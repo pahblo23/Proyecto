3ee390dab3b5a3300c2051cf42583b7d233eb71b
Torelli <torelli23@gmail.com>
2019-11-16 15:27:43 -0600

    se quita un conflicto

--- src/negocio/Pulpe.java
+++ src/negocio/Pulpe.java
@@ -4,25 +4,11 @@ package negocio;
 <<<<<<< HEAD
  * Clase con métodos vacíos para sobrescribirlos según la función de cada clase hija
  * instanciando esta clase para llamar a los métodos desde el menú
-=======
- * Clase con métodos vacíos para sobrescribirlos según la función de cada clase
- * hija instanciando esta clase para llamar a los métodos desde el menú
- *
->>>>>>> 7a71abde69223b33dcf7c7741c357eee1afcd90e
  * @author Pablo Díaz
  * @version 1.2
  */
 public class Pulpe {
-<<<<<<< HEAD
-    public  void registrar(){};
-    public void ver(){};
-    public void modificar(){};
-    public void eliminar(){};
-    public void cargar(){};
-    public void guardar(){};
-      
-}
-=======
+
 
     /**
      * Metodo para registrar
@@ -53,4 +39,3 @@ public class Pulpe {
 ;
     
 }
->>>>>>> 7a71abde69223b33dcf7c7741c357eee1afcd90e
