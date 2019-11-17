91df503b68552afdc4f0161c393cd4cc567b261d
AnthonyGomez97 <Anthony_gomez97@hotmail.com>
2019-11-16 18:22:10 -0600

    Se agrego Venta y ManejoVenta

--- src/negocio/ManejoDato.java
+++ src/negocio/ManejoDato.java
@@ -14,6 +14,7 @@ public class ManejoDato implements Serializable {
 
     public static ArrayList<Cliente> listadoClientes = new ArrayList<>();//Se crea una lista de tipo objetocliente
     public static ArrayList<Inventario> InventarioD = new ArrayList<>();
+    public static ArrayList<Venta> VentaD = new ArrayList<>();
     public static ArrayList<negocio.Proveedor> Proveedores = new ArrayList<>();
     public static String rutaArchivo = "C:\\Users\\Usuario\\Documents\\NetBeansProjects\\PulpePOS\\src\\datos\\Datos.txt";
     public static Archivo<Inventario> archivo = new Archivo<>();
